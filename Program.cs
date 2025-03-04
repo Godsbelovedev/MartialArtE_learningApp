c120c7c45d58f44523ba923766abe9c15895fd8d
Shaqoo <ShakirullahOhio@gmail.com>
2025-03-04 16:02:20 +0100

    Second Commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Enter your first number");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter your first number");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter your first number");
+int num3 = int.Parse(Console.ReadLine());
+Console.WriteLine(SumThreeIntegers(num1,num2,num3));
+static int SumThreeIntegers(int firstNumber, int secondNumber, int thirdNumber)
+{
+    return firstNumber+secondNumber+thirdNumber;
+}
